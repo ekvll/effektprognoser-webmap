b2a917a6e5292f3b17acfbd5ee1b3962fae14811
Erik Lindvall <erik.k.lindvall@gmail.com>
2025-06-03 23:06:26 +0200

    work on legend layout

--- assets/js/legend.js
+++ assets/js/legend.js
@@ -2,29 +2,20 @@ import { svgDiagonalLines } from "./patterns.js";
 import { map, selectedValues } from "./map.js";
 import { getBoundariesAndColors } from "./boundaries_colors.js";
 
-export function updateLegend() {
-  const legend = L.control({ position: "bottomright" });
+let negativeValues = null;
 
-  console.log(legend);
-
-  legend.onAdd = function (map) {
-    const div = L.DomUtil.create("div", "legend");
-    div.innerHTML = "Legend";
-
-    const { boundaries, colors } = getBoundariesAndColors();
-
-    for (let i = 0; i < boundaries.length; i++) {
-      if (boundaries[i].min !== 10e6 - 1) {
-        div.innerHTML += `
-<div style="display: flex; align-items: center; margin-bottom: 4px;">
-<div style="width: 20px; height: 20px; background: ${colors[i]}; margin-right: 8px; border: 1px solid #000;"></div> 
-<span>${boundaries[i].min} - ${boundaries[i].max}</span>
-</div>
+const svgDot = `
+<svg width="20" height="20" xmlns="http://www.w3.org/2000/svg">
+  <defs>
+    <pattern id="dotPattern" patternUnits="userSpaceOnUse" width="5" height="5">
+      <circle cx="2.5" cy="2.5" r="1" fill="black" />
+    </pattern>
+  </defs>
+  <rect width="20" height="20" fill="url(#dotPattern)" />
+</svg>
 `;
-      }
-    }
-    if (selectedValues.prognos !== "effektbehov") {
-      const svgCode = `
+
+const svgCode = `
 <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg">
   <defs>
     <pattern id="diagonalLines" patternUnits="userSpaceOnUse" width="5" height="5" patternTransform="rotate(45)">
@@ -34,10 +25,72 @@ export function updateLegend() {
   <rect width="20" height="20" fill="url(#diagonalLines)" />
 </svg>
 `;
+
+export function updateLegend() {
+  const legend = L.control({ position: "bottomright" });
+
+  console.log(legend);
+
+  legend.onAdd = function (map) {
+    const div = L.DomUtil.create("div", "legend");
+    if (selectedValues.prognos === "effektbehov") {
+      div.innerHTML = "Effektbehov (MW)";
+    } else if (selectedValues.prognos === "ebd") {
+      div.innerHTML = "Tillkommande<br>effektbehov (MW)";
+    } else if (selectedValues.prognos === "ebp") {
+      div.innerHTML = "Tillkommande<br>effektbehov (%)";
+    } else if (selectedValues.prognos === "elanvandning") {
+      div.innerHTML = "Tillkommande<br>elanvändning (MWh)";
+    } else if (selectedValues.prognos === "eap") {
+      div.innerHTML = "Tillkommande<br>elanvändning (%)";
+    }
+
+    const { boundaries, colors } = getBoundariesAndColors();
+
+    for (let i = 0; i < boundaries.length; i++) {
+      // Dont display colors & boundaries devoted to 'Ny bebyggelse' (10e6 - 1)
+      // and negative values
+      if (boundaries[i].min !== 10e6 - 1 && boundaries[i].min !== -Infinity) {
+        if (boundaries[i].max === 10e6 - 1) {
+          div.innerHTML += `
+<div style="display: flex; align-items: center; margin-bottom: 4px;">
+<div style="width: 20px; height: 20px; background: ${colors[i]}; margin-right: 8px; border: 1px solid #000;"></div> 
+<span>> ${boundaries[i].min}</span>
+</div>
+`;
+        } else {
+          div.innerHTML += `
+<div style="display: flex; align-items: center; margin-bottom: 4px;">
+<div style="width: 20px; height: 20px; background: ${colors[i]}; margin-right: 8px; border: 1px solid #000;"></div> 
+<span>${boundaries[i].min} - ${boundaries[i].max}</span>
+</div>
+`;
+        }
+        // Update the reference if negative values are present
+      } else if (boundaries[i].min === -Infinity) {
+        negativeValues = true;
+      }
+    }
+
+    // If negative values are present and prognos is not effektbehov
+    // then add a square for dot pattern (negative values)
+    if (negativeValues === true && selectedValues.prognos !== "effektbehov") {
+      div.innerHTML += svgDot + "< 0";
+      negativeValues = null;
+    } else if (selectedValues.prognos === "effektbehov") {
+      div.innerHTML += `
+<div style="display: flex; align-items: center; margin-bottom: 4px;">
+<div style="width: 20px; height: 20px; background: ${colors[colors.length - 1]}; margin-right: 8px; border: 1px solid #000;"></div> 
+<span>< ${boundaries[boundaries.length - 1].max}</span>
+</div>
+`;
+    }
+
+    if (selectedValues.prognos !== "effektbehov") {
       if (selectedValues.raps === "transport") {
-        div.innerHTML += svgCode + "Ny laddinfra";
+        div.innerHTML += "<br>" + svgCode + "Ny laddinfra";
       } else {
-        div.innerHTML += svgCode + "Ny bebyggelse";
+        div.innerHTML += "<br>" + svgCode + "Ny bebyggelse";
       }
     }
 
